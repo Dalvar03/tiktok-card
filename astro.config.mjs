2c39736dff24acbbc9cb2c8259c157a46178d4ab
Dalvar03 <vladantonyak2@gmail.com>
2026-08-05 19:32:13 +0300

    Add deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://_stoc.chg_.github.io",
+  base: "/tiktok-card",
+});
